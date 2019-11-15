9caaea342a45964f3f3e9545fc3705ce99b7b58c
GabMat97 <gab.matos@outlook.com>
2019-11-15 14:58:19 +0000

    Failing delete tests

--- backend/test/app.test.js
+++ backend/test/app.test.js
@@ -4,23 +4,21 @@ const MessageApp = require("../app.js")
 let data;
 
 describe("message API endpoint tests", function(){
-  beforeEach(function(){
+  it("posts a message", function(done) {
     data = {
       content: "hi world"
     };
-  })
-  it("posts a message", function(done) {
     const res = request(MessageApp)
     .post("/message")
     .send(data)
     .set("Accept", "application/json")
     res.expect(200)
     .end(function(err, res) {
-        if (err) {
-          return done(err)
-        }
-        expect(res.body.length).to.equal(1)
-        done()
+      if (err) {
+        return done(err)
+      }
+      expect(res.body.length).to.equal(1)
+      done()
     })
   })
   it("gets all messages", function(done) {
@@ -28,11 +26,28 @@ describe("message API endpoint tests", function(){
     .get("/")
     res.expect(200)
     .end(function(err, res) {
+      if (err) {
+        return done(err)
+      }
+      expect(res.body.length).to.equal(1)
+      done()
+    })
+    it("deletes a message", function(done) {
+      data = {
+        id: 0
+      };
+      const res = request(MessageApp)
+      .delete("/delete/0")
+      .send(data)
+      .set("Accept", "application/json")
+      res.expect(200)
+      .end(function(err, res) {
         if (err) {
           return done(err)
         }
         expect(res.body.length).to.equal(1)
         done()
-    })
+      })
+    // })
   })
 })
