0cc6e29add335ef44c175d57110366b8ab888c14
GabMat97 <gab.matos@outlook.com>
2019-11-19 17:28:19 +0000

    Completed passing frontend tests

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,28 @@
 import React from 'react';
 import './App.css';
 
-function App() {
+class MessageApp extends React.Component {
+  render(){
+
   return (
     <div className="App">
+      <textarea
+        id='message_box'>
+        </textarea>
+        <br/>
+        <button
+        type="button"
+        name="Submit"
+        id="submit">
+        Submit
+      </button>
+      <ul
+      id="message_list">
+        Messages
+      </ul>
     </div>
   );
 }
+}
 
-export default App;
+export default MessageApp;
