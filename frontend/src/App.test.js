d920cb3d30bf466313aac9b9ffa425095f84f3c0
Tom Russell <TR115251@hotmail.co.uk>
2019-11-20 13:34:10 +0000

    transfer

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -7,21 +7,34 @@ import Adapter from 'enzyme-adapter-react-16';
 import { mount } from 'enzyme'
 
 Enzyme.configure({ adapter: new Adapter()})
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<MessageApp />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
 
-it('has textbox', () => {
-  const component = mount(<MessageApp/>);
-  expect(component.exists('textarea#message_box')).toBe(true);
-});
-it('has submit button', () => {
-  const component = mount(<MessageApp/>);
-  expect(component.exists('button#submit')).toBe(true);
-});
-it('has message list', () => {
-  const component = mount(<MessageApp/>);
-  expect(component.exists('ul#message_list')).toBe(true);
+describe('App', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<MessageApp />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  it('has textbox', () => {
+    const component = mount(<MessageApp/>);
+    expect(component.exists('textarea#message_box')).toBe(true);
+  });
+
+  it('has submit button', () => {
+    const component = mount(<MessageApp/>);
+    expect(component.exists('button#submit')).toBe(true);
+  });
+
+  it('has message list', () => {
+    const component = mount(<MessageApp/>);
+    expect(component.exists('ul#message_list')).toBe(true);
+  });
+
+  it('posts data on submit', () => {
+    const component = mount(<MessageApp/>);
+    component.find('textarea#message_box').simulate('change', { target: { value: 'Hello' } })
+    component.find('form').simulate('submit')
+    expect(fetchMock.post).toHaveBeenCalledWith("http://localhost:3001/message", {"content": "Hello"});
+  });
+
 });
